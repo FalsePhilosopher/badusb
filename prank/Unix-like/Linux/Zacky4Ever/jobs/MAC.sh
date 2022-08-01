334e0ad540ea9fff8232ac2a8464aba5c2848582
FalsePhilosopher <joshgarland90@gmail.com>
2022-07-31 18:53:18 -0600

    cleanup

--- prank/Unix-like/Linux/Zacky4Ever/jobs/MAC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Set a random Zack Efron desktop picture in macOS
-# Parameter $1: Root Folder of the Zacky Prank
-
-ROOT_FOLDER=$1
-TARGET_FILE=$(sh $ROOT_FOLDER/.randomizer.sh $ROOT_FOLDER)
-osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$TARGET_FILE\""
-killall Dock
--- prank/Unix-like/Linux/Zacky4Ever/randomizer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-# Selects a random file from the given folder
-# Parameter $1 -> Source Folder
-
-ROOT_FOLDER=$1
-files=($ROOT_FOLDER/*)
-printf "%s\n" "${files[RANDOM % ${#files[@]}]}"
